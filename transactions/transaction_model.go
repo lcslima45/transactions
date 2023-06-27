3cb36a9cb5e4731a887f321c836a4640935556fb
Lucas Lima de Sousa <30378522+lcslima45@users.noreply.github.com>
2023-06-26 23:37:10 -0300

    update

--- transactions/transaction_model.go
+++ transactions/transaction_model.go
@@ -2,12 +2,12 @@ package transactions
 
 //Model of the transaction that is in the database
 type Transaction struct {
-	ID         int
-	Cardholder string
-	Merchant   string
-	Acquirer   string
-	Brand      string
-	Issuer     string
+	ID         int    //Identificador no banco de dados
+	Cardholder string //Nome do dono do cartão
+	Merchant   string //Local de vendas onde a operação foi realizada
+	Acquirer   string //Instituição que possibilida ao local de vendas aceitar cartões
+	Brand      string //Bandeira do cartão
+	Issuer     string //Banco ao qual o cartão do Cardholder está ligado
 }
 
 //Interface that implements the service that is called by the handler and manipulates the Repository
